cd8ddcd47e92c8afbf64a8f8c423c3459d7bd66d
sai <sai@163.com>
2019-08-16 00:13:43 +0800

    first commit

---
+++ src/main/java/com/bea/spring/springspringboot/SpringSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.bea.spring.springspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSpringbootApplication.class, args);
+    }
+
+}
